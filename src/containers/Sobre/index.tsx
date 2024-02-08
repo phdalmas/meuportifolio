f442b05fe31ddf82750d4637cbfd53c45cdcfa57
Pedro Dalmas <phdalforno@gmail.com>
2024-02-08 13:48:07 -0300

    ajustes bio

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,18 +6,17 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
-      Olá! Me chamo Pedro, tenho 30 anos e há 5 anos atuo como Engenheiro Civil.
-      Com experiência nesta área pude desenvolver importantes habilidades
-      pessoais como comunicação, disciplina, resolução de problemas,
-      gerenciamento sob pressão e gestão de projetos de alta duração. Nesse
-      sentido, desenvolvi meu perfil técnico e analítico nas diversas situações
-      vivenciadas na rotina de gestão de projetos e obras. Gosto de ser
-      desafiado e busco resolver problemas com objetividade. Possuo interesse na
-      área da tecnologia desde muito cedo, tanto que meu início acadêmico foi na
-      área da engenharia elétrica. Hoje busco novos desafios na minha carreira,
-      e tenho me dedicado a desenvolver habilidades na área de tecnologia. Estou
-      cursando Analise e Desenvolvimento de Sistema na Uniámerica, e estou muito
-      entusiasmado com as novas oportunidades que a área oferece.
+      Olá. Me chamo Pedro, tenho 30 anos e sou de Passo Fundo/RS. Sou engenheiro
+      civil de formação e atuo na área há 5 anos. A experiência nesta área
+      acrescentou muito no meu desenvolvimento pessoal, como minha comunicação,
+      a capacidade de resolução de problemas, gerenciamento sob pressão e gestão
+      de projetos. Gosto de estar aprendendo novas habilidades e me desafiando.
+      Para além da atuação como engenheiro civil, tenho interesse na área da
+      tecnologia desde muito cedo, tanto que meu início acadêmico foi na área da
+      engenharia elétrica. Hoje busco novas oportunidades para minha carreira, e
+      tenho me desafiado na área de tecnologia. Estou cursando Analise e
+      Desenvolvimento de Sistema pela Uniámerica, e também Desenvolvimento Full
+      Stack em Python pela EBAC e procurando novos desafios profissionais.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=phdalmas&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
