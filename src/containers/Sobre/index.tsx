5af301c0ed2f67cb68ab4cc9d8a7d6a381f52c35
Pedro <phdalforno@gmail.com>
2025-03-24 17:18:01 -0300

    Adjusts on about me

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,19 +6,18 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
-      Olá. Me chamo Pedro, tenho 31 anos e sou de Passo Fundo/RS. Sou engenheiro
-      civil de formação desde 2019, e estou cursando Análise e Desenvolvimento
-      de Sistema pela Uniámerica. Desde março de 2024 trabalho na empresa Onbox
-      Design, da Austrália. Tenho trabalhado com TypeScript, JavaScript, React,
-      C# e outras linguagem na criação de um software para a área de
-      engenharia/arquitetura. Tenho experiencia na parte de testes unitários e
-      integração e também em PHP e Python por meio de projetos próprios. A
-      experiência na área de engenharia, onde atuei por 5 anos, acrescentou
-      muito no meu desenvolvimento pessoal, como minha comunicação, a capacidade
-      de resolução de problemas, gerenciamento sob pressão e gestão de projetos.
-      Gosto de estar aprendendo novas habilidades e me desafiando. Hoje busco
-      novas oportunidades para minha carreira, e tenho me desafiado na área de
-      tecnologia.
+      Olá. Me chamo Pedro, tenho 31 anos e sou de Passo Fundo/RS. Estou cursando
+      Análise e Desenvolvimento de Sistema pela Uniámerica. Desde março de 2024
+      trabalho na empresa Onbox Design, da Austrália. Tenho trabalhado com
+      TypeScript, JavaScript, React, C# e outras linguagem na criação de um
+      software para a área de engenharia/arquitetura. Tenho experiencia na parte
+      de testes unitários e integração e também em PHP e Python por meio de
+      projetos próprios. A experiência na área de engenharia, onde atuei por 5
+      anos, acrescentou muito no meu desenvolvimento pessoal, como minha
+      comunicação, a capacidade de resolução de problemas, gerenciamento sob
+      pressão e gestão de projetos. Gosto de estar aprendendo novas habilidades
+      e me desafiando. Hoje busco novas oportunidades para minha carreira, e
+      tenho me desafiado na área de tecnologia.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=phdalmas&show_icons=true&theme=dracula" />
